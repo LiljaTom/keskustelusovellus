12409687064ff64ba5626d837bca3f9cfb175bb5
Tomi Lilja <tomi.lilja@helsinki.fi>
2024-02-18 17:04:34 +0200

    Add threads.py

---
+++ thread.py
@@ -0,0 +1,23 @@
+from db import db
+from sqlalchemy.sql import text
+import login
+
+def createThread(groupId, content, title):
+    userId = login.getUserId()
+
+    if userId == 0:
+        return False
+    
+    try: 
+        sql = text("INSERT INTO threads (content, group_id, title, user_id) VALUES (:content, :group_id, :title, :user_id)")
+        db.session.execute(sql, {"content":content, "group_id":groupId, "title":title, "user_id": userId})
+        db.session.commit()
+        return True
+    except:
+        return False
+    
+def getThreadsInGroup(groupId):
+    sql = text("SELECT title FROM threads WHERE group_id=:group_id")
+    result = db.session.execute(sql, {"group_id": groupId})
+
+    return result.fetchall()
