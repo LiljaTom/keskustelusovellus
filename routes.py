08ed720b3fa3495845324aceba9e4baa17db3c7d
Tomi Lilja <tomi.lilja@helsinki.fi>
2024-03-03 13:01:21 +0200

    Refactor login route

--- routes.py
+++ routes.py
@@ -16,8 +16,7 @@ def redirectToThread(threadId, groupId):
 
 @app.route("/login", methods=["GET"])
 def getLogin():
-    if request.method == "GET":
-       return render_template("login.html")
+    return render_template("login.html")
     
 @app.route("/login", methods=["POST"])
 def postLogin():
